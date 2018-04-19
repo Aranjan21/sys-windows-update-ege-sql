0374c61018b276f7faf5a56d9ccc3f3bf47f1010
Gaetano, Anthony <agaetano@cvent.com>
2018-04-19 13:41:37 -0400

    removing line 69 using sql_script...sql_script variable DNS

--- main.groovy
+++ main.groovy
@@ -66,7 +66,6 @@ def call(def base) {
     }
 
     ps_script = ps_script['message']
-    sql_script = sql_script['message']
 
     /* Run the PowerShell script */
     for (Integer i = 0; i < list_of_ege_servers.size(); i++) {
