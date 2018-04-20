9e915a6cc7618b7ffde262361897133c5aa54adc
Gaetano, Anthony <agaetano@cvent.com>
2018-04-20 12:15:12 -0400

    updating output from ps1

--- main.groovy
+++ main.groovy
@@ -53,8 +53,6 @@ def call(def base) {
 
     /* Read the PowerShell file for the workflow */
 
-    list_of_ege_servers = ['ap20-ege-101']
-
     this_base.log("getting PS file")
 
     def ps_script = base.read_wf_file('sys-windows-update-ege-sql', 'ege-drop-and-recreate-assemblies.ps1')
